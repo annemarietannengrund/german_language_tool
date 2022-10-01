cfe45a486affb5675158dadb65e65d4128165ba4
Annemarie Tannengrund <annemarie.tannengrund@gmail.com>
2022-10-01 16:37:17 +0200

    simpler translate/build process

--- build_release.py
+++ build_release.py
@@ -1,4 +1,4 @@
-
+import PyInstaller.__main__
 import googletrans
 from googletrans import Translator
 from progress.bar import Bar
@@ -6,18 +6,21 @@ import json
 
 TRANSLATOR = Translator()
 
-def list_available_languages()->None:
+
+def list_available_languages() -> None:
     for language_key, language_name in googletrans.LANGUAGES.items():
         print(language_key, language_name)
 
-def load_language_json(path_to_json:str)->dict:
+
+def load_language_json(path_to_json: str) -> dict:
     with open(path_to_json, "r") as f:
         try:
             return json.loads(f.read())
         except Exception as exc:
             print(exc)
 
-def create_language_from_dict(src_dict:dict, src_lang:str, tgt_lang:str)->dict:
+
+def create_language_from_dict(src_dict: dict, src_lang: str, tgt_lang: str) -> dict:
     new_language = {}
     bar = Bar(f"Processing {src_lang} -> {tgt_lang}", max=len(src_dict))
     for key, value in src_dict.items():
@@ -27,20 +30,23 @@ def create_language_from_dict(src_dict:dict, src_lang:str, tgt_lang:str)->dict:
     bar.finish()
     return new_language
 
-def get_gt(value, src_lang, tgt_lang)->str:
+
+def get_gt(value, src_lang, tgt_lang) -> str:
     return TRANSLATOR.translate(value, src=src_lang, dest=tgt_lang).text
 
-def dump_to_json(data:dict, tgt_path:str)->None:
+
+def dump_to_json(data: dict, tgt_path: str) -> None:
     with open(tgt_path, 'w') as f:
         f.write(json.dumps(data, indent=4))
 
-def create_i18n_files_from_config(config:dict)->None:
+
+def translate_content(config: dict) -> None:
     src_lang = config.get("src_path").format(config.get("src_language"))
     print(f"parsing source json: {src_lang}")
     src_language_dict = load_language_json(path_to_json=src_lang)
     languages_compiled = {}
     compiled_src = {}
-    for key, item in src_language_dict.items():
+    for key, item in src_language_dict.get('translation_base').items():
         compiled_src[key] = item
     compiled_src["language_name"] = googletrans.LANGUAGES.get(config.get("src_language"))
     languages_compiled[config.get("src_language")] = compiled_src
@@ -56,16 +62,21 @@ def create_i18n_files_from_config(config:dict)->None:
         target_language["language_name_english"] = eng_lang_name
         target_language["language_name_native"] = get_gt(eng_lang_name, "en", target_language_code)
         languages_compiled[target_language_code] = target_language
-    dump_to_json(languages_compiled, "translated.json")
+    reserved = src_language_dict.get("translation_reserved")
+
+    dump_to_json({"translation_base": languages_compiled, "reserved": reserved}, "assets/translated.json")
+
+def get_translation_config() -> dict:
+    return {
+        "src_language": "en",
+        "tgt_languages": ["de"],
+        "src_path": "translation.base.json"
+    }
+
 
 if __name__ == "__main__":
-    config = {
-        "src_language": "en",
-        "tgt_languages": ["de", "en",],#"bg", "fr", "it", "pl", "es", "fa"], # use googletrans.LANGUAGES.keys() for all 107 languages
-        "src_path": "translation.en.json"
-    }
-    create_i18n_files_from_config(config)
-    #for key, lang in googletrans.LANGUAGES.items():
-        #print(key, lang)
-    #lang = load_language_json(config.get("src_path"))
-    #print(lang)
+    # list_available_languages()
+    translate_content(get_translation_config())
+    PyInstaller.__main__.run([
+        'main.spec',
+    ])
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import json
-import platform
+from platform import system
 import tkinter as tk
 from os.path import splitext, realpath, join as os_join, isfile
 
@@ -12,7 +12,7 @@ from page_toc import get_toc
 class GermanLanguageTool(tk.Tk):
     lang: str
     title = "German Language Tool"
-    translation_file = "translated.json"
+    translation_file = "assets/translated.json"
     translate_map: dict
     frameregister = {}
 
@@ -33,11 +33,13 @@ class GermanLanguageTool(tk.Tk):
         self.translate_map = {}
         self.translate_raw_map = {}
         self.available_languages = {}
-        self.platform = platform.system()
+        self.platform = system()
+        self.reserved_words = {}
         self.set_language(lang)
         self.init_mainframe_container()
         self.init_page_frames()
         self.show_frame(self.default)
+
 
     def join_path(self, part1, part2):
         return os_join(part1, part2)
@@ -72,32 +74,29 @@ class GermanLanguageTool(tk.Tk):
         self.translate_raw_map = {}
         path = self.get_filepath(self.translation_file)
         with open(path, "r", encoding="utf-8") as f:
-            translations = json.loads(f.read())
+            filecontent = json.loads(f.read())
+            self.translations = filecontent.get("translation_base")
+            self.replace_words = filecontent.get("reserved")
         if not self.available_languages:
-            for code, content in translations.items():
+            for code, content in self.translations.items():
                 self.available_languages[code] = {
                     "language_name_english": content.get("language_name_english"),
                     "language_name_native": content.get("language_name_native"),
                 }
-        self.translate_raw_map = translations.get(lang)
+        self.translate_raw_map = self.translations.get(lang)
         self.init_string_var_translation_components()
 
     def update_translation(self, key, new_text):
         self.translate_map[key].set(new_text)
 
+    def replace_reserved_words(self, translation):
+        for key, replacement in self.replace_words.items():
+            translation = translation.replace(key, replacement)
+        return translation
+
     def init_string_var_translation_components(self):
         for key, translation in self.translate_raw_map.items():
-            translation = translation.replace('__UUNNDD__', 'und')
-            translation = translation.replace('__SZ__', 'ß')
-            translation = translation.replace('__DREI__', 'Drei')
-            translation = translation.replace('__ZWEI__', 'Zwei')
-            translation = translation.replace('__VIER__', 'Vier')
-            translation = translation.replace('__DREISZIG__', 'Dreißig')
-            translation = translation.replace('__20__', 'Zwanzig')
-            translation = translation.replace('__30__', 'Dreißig')
-            translation = translation.replace('__90__', 'Neunzig')
-            translation = translation.replace('__HUNDERT__', 'hundert')
-            translation = translation.replace('__TAUSEND__', 'tausend')
+            translation = self.replace_reserved_words(translation)
             if key in self.translate_map.keys():
                 self.update_translation(key, translation)
             else:
